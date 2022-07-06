88d3149ba3980b2fce35077b0449a99a797fac4f
Emmanuel Bondzie-Selby <45995752+nuel123@users.noreply.github.com>
2022-07-06 23:31:08 +0800

    Add files via upload

---
+++ js/customizer.js
@@ -0,0 +1,10 @@
+//-------- Calendar js --------//
+$('#calendar').fullCalendar({
+  header: {
+    left: 'prev, next, today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay,listWeek'
+  },
+  navLinks: true,
+  eventLimit: true,
+})
